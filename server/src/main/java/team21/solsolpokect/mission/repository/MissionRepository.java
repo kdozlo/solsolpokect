9c864ae454a366c71070df731371cdc0b3da0924
kdozlo <eodud5730@naver.com>
2023-09-10 20:51:18 +0900

    Feat : MissionRepository - findAllbyUserId 구현

--- server/src/main/java/team21/solsolpokect/mission/repository/MissionRepository.java
+++ server/src/main/java/team21/solsolpokect/mission/repository/MissionRepository.java
@@ -1,7 +1,14 @@
 package team21.solsolpokect.mission.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import team21.solsolpokect.mission.entity.Mission;
 
+import java.util.List;
+
 public interface MissionRepository extends JpaRepository<Mission,Long> {
+
+    @Query("SELECT m FROM Mission m WHERE m.user.userId = :userId AND m.deletedDate IS NULL  ORDER BY m.createdAt DESC")
+    List<Mission> findAllByUserId(@Param("userId")Long userId);
 }
