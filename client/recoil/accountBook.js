8d437388f3f68430bc637f8e9fe7e5e6ce5060da
leewooseong <sitpo96@naver.com>
2023-09-11 07:44:35 +0900

    Feat: isFeedbackVisibleAtom recoil state 추가
    - 피드백 내용 보여주기 여부를 판단하는 recoil state 추가

--- client/recoil/accountBook.js
+++ client/recoil/accountBook.js
@@ -15,3 +15,8 @@ export const isPickerVisibleAtom = atom({
   key: 'accountPickerModalBoolean',
   default: false,
 });
+
+export const isFeedbackVisibleAtom = atom({
+  key: 'accountFeedbackModalBoolean',
+  default: false,
+});
