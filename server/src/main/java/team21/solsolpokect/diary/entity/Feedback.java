f804a24930b7d00aa95eeead1252c7b2cbee082c
Seyoung Oh <osy9536@naver.com>
2023-09-11 21:35:25 +0900

    Feat : 정적 팩토리 메서드 구현

--- server/src/main/java/team21/solsolpokect/diary/entity/Feedback.java
+++ server/src/main/java/team21/solsolpokect/diary/entity/Feedback.java
@@ -1,8 +1,10 @@
 package team21.solsolpokect.diary.entity;
 
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
 import team21.solsolpokect.common.entity.BaseTime;
 import team21.solsolpokect.user.entity.Users;
 
@@ -11,6 +13,7 @@ import javax.persistence.*;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@SQLDelete(sql = "UPDATE post set deleted_at = CONVERT_TZ(NOW(), 'UTC', 'Asia/Seoul') where post_id = ?")
 public class Feedback extends BaseTime {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,4 +25,21 @@ public class Feedback extends BaseTime {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     private Users users;
+
+    @Builder
+    private Feedback(String contents, Users users) {
+        this.contents = contents;
+        this.users = users;
+    }
+
+    public static Feedback of(String contents, Users users) {
+        return builder()
+                .contents(contents)
+                .users(users)
+                .build();
+    }
+
+    public void update(String contents) {
+        this.contents = contents;
+    }
 }
