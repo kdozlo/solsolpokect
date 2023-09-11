bd8b02c8d99dc2a7c7e99c0eff62b96d1c61dffc
Seyoung Oh <osy9536@naver.com>
2023-09-11 21:37:25 +0900

    Feat : 삭제 데이터 처리 쿼리 추가

--- server/src/main/java/team21/solsolpokect/diary/repository/DiaryRepository.java
+++ server/src/main/java/team21/solsolpokect/diary/repository/DiaryRepository.java
@@ -1,7 +1,14 @@
 package team21.solsolpokect.diary.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import team21.solsolpokect.diary.entity.Diary;
 
+import java.util.Optional;
+
 public interface DiaryRepository extends JpaRepository<Diary,Long> {
+
+    @Query("SELECT p FROM Post p WHERE p.postId = :postId AND p.deletedAt IS NULL")
+    Optional<Diary> findById(@Param("postId") Long postId);
 }
--- server/src/main/java/team21/solsolpokect/diary/repository/FeedbackRepository.java
+++ server/src/main/java/team21/solsolpokect/diary/repository/FeedbackRepository.java
@@ -1,7 +1,23 @@
 package team21.solsolpokect.diary.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import team21.solsolpokect.diary.entity.Feedback;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
 public interface FeedbackRepository extends JpaRepository<Feedback,Long> {
+
+    @Query("SELECT p FROM Post p WHERE p.postId = :postId AND p.deletedAt IS NULL")
+    Optional<Feedback> findById(@Param("postId") Long postId);
+
+    @Query("SELECT p FROM Post p WHERE p.postId = :postId AND p.deletedAt IS NULL")
+    List<Feedback> findAllByUserIdAndCreatedAtBetween(
+            Long userId,
+            LocalDate startOfMonth,
+            LocalDate endOfMonth
+    );
 }
