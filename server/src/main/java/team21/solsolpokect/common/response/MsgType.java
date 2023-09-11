eefd14bce63e7f96c1fee52e52873cd75f138e32
Seyoung Oh <osy9536@naver.com>
2023-09-11 21:53:11 +0900

    Chore : MsgType 세미콜론 삭제

--- server/src/main/java/team21/solsolpokect/common/response/MsgType.java
+++ server/src/main/java/team21/solsolpokect/common/response/MsgType.java
@@ -18,7 +18,7 @@ public enum MsgType {
     MISSION_ALLOW_PICTURE_SUCCESSFULLY("도전 과제 인증 사진 전송 성공"),
     MISSION_COMPLETE_SUCCESSFULLY("도전 과제 완료 전송 성공"),
     FEEDBACK_UPDATE_SUCCESSFULLY("피드백 수정 성공"),
-    FEEDBACK_CHECK_SUCCESSFULLY("피드백 조회 성공");
+    FEEDBACK_CHECK_SUCCESSFULLY("피드백 조회 성공"),
     AUTOTRANSFER_CREATE_SUCCESSFULLY("자동 이체 생성 성공"),
     AUTOTRANSFER_LIST_SUCCESSFULLY("자동 이체 목록 조회 성공"),
     AUTOTRANSFER_UPDATE_SUCCESSFULLY("자동 이체 수정 성공"),
