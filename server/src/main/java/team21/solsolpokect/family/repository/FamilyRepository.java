f36878c6dfc67fb9e109f5c4c36fd5add30ef849
kdozlo <eodud5730@naver.com>
2023-09-12 10:55:02 +0900

    Feat : FamilyRepository Jpa 상속, UsersRepository findByUserId 구현

--- server/src/main/java/team21/solsolpokect/family/repository/FamilyRepository.java
+++ server/src/main/java/team21/solsolpokect/family/repository/FamilyRepository.java
@@ -1,4 +1,7 @@
 package team21.solsolpokect.family.repository;
 
-public interface FamilyRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import team21.solsolpokect.family.entity.Family;
+
+public interface FamilyRepository extends JpaRepository<Family, Long> {
 }
--- server/src/main/java/team21/solsolpokect/user/repository/UsersRepository.java
+++ server/src/main/java/team21/solsolpokect/user/repository/UsersRepository.java
@@ -1,7 +1,13 @@
 package team21.solsolpokect.user.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import team21.solsolpokect.user.entity.Users;
 
+import java.util.Optional;
+
 public interface UsersRepository extends JpaRepository<Users, Long> {
+    @Query("SELECT u FROM Users u WHERE u.deletedAt IS NULL")
+    Optional<Users> findByUserId(String userId);
+
 }
