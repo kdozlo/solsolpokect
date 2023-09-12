3f7f09fcd2c27faafff7b9c8af915f298ccb1a6a
kdozlo <eodud5730@naver.com>
2023-09-12 09:39:08 +0900

    Feat : familyDetail api 생성

--- server/src/main/java/team21/solsolpokect/family/controller/FamilyController.java
+++ server/src/main/java/team21/solsolpokect/family/controller/FamilyController.java
@@ -2,14 +2,12 @@ package team21.solsolpokect.family.controller;
 
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import team21.solsolpokect.common.response.ApiResponseDto;
 import team21.solsolpokect.common.response.MsgType;
 import team21.solsolpokect.common.response.ResponseUtils;
 import team21.solsolpokect.family.dto.request.FamilyCreateRequestDto;
+import team21.solsolpokect.family.dto.response.FamilyInfoResponseDto;
 import team21.solsolpokect.family.service.FamilyService;
 
 @RestController
@@ -23,5 +21,10 @@ public class FamilyController {
     public ApiResponseDto<Void> familyCreate(@RequestBody FamilyCreateRequestDto familyCreateRequestDto) {
         familyService.missionCreate(familyCreateRequestDto);
         return ResponseUtils.ok(MsgType.FAMILY_CREATE_SUCCESSFULLY);
+    }
+
+    @GetMapping("/info/{family-id}")
+    public ApiResponseDto<FamilyInfoResponseDto> familyDetail(@PathVariable("family-id") Long familyId) {
+        return ResponseUtils.ok(familyService.familyDetail(familyId), MsgType.FAMILY_DETAIL_SUCCESSFULLY);
     }
 }
