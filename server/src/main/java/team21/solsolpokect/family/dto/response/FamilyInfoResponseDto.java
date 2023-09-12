5d7d27391964d9775a7ce945c1b9f7cf11055d67
kdozlo <eodud5730@naver.com>
2023-09-12 09:31:47 +0900

    Feat : FamilyCreateRequestDto, FamilyInfoResponseDto 생성

--- server/src/main/java/team21/solsolpokect/family/dto/response/FamilyInfoResponseDto.java
+++ server/src/main/java/team21/solsolpokect/family/dto/response/FamilyInfoResponseDto.java
@@ -1,4 +1,11 @@
 package team21.solsolpokect.family.dto.response;
 
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
 public class FamilyInfoResponseDto {
+    List<Long> userId;
+    String familyName;
 }
