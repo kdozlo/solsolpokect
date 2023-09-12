bbd55cb39e3a86c11ffcc6e14bd528913c3b034b
kdozlo <eodud5730@naver.com>
2023-09-12 15:16:43 +0900

    Chore : FamilyRepository - delete unnecessary import package

--- server/src/main/java/team21/solsolpokect/family/repository/FamilyRepository.java
+++ server/src/main/java/team21/solsolpokect/family/repository/FamilyRepository.java
@@ -1,10 +1,8 @@
 package team21.solsolpokect.family.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import team21.solsolpokect.family.entity.Family;
-import team21.solsolpokect.mission.entity.Mission;
-import team21.solsolpokect.user.entity.Users;
+
 
 import java.util.List;
 
