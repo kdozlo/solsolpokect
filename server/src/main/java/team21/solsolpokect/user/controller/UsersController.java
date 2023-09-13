0d3a7e6dc83778af3b8121bed622a522401a606d
Seyoung Oh <osy9536@naver.com>
2023-09-14 00:00:16 +0900

    Feat : 회원가입, 로그인 구현
    close #59

--- server/src/main/java/team21/solsolpokect/user/controller/UsersController.java
+++ server/src/main/java/team21/solsolpokect/user/controller/UsersController.java
@@ -1,4 +1,38 @@
 package team21.solsolpokect.user.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import team21.solsolpokect.common.response.ApiResponseDto;
+import team21.solsolpokect.common.response.MsgType;
+import team21.solsolpokect.common.response.ResponseUtils;
+import team21.solsolpokect.user.dto.request.LoginRequestDto;
+import team21.solsolpokect.user.dto.request.SignupRequestDto;
+import team21.solsolpokect.user.service.UsersService;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("api/users")
+@RequiredArgsConstructor
 public class UsersController {
+
+    private final UsersService usersService;
+
+    @PostMapping("/signup")
+    public ApiResponseDto<Void> signup(@Valid @RequestBody SignupRequestDto requestDto) {
+
+        usersService.signup(requestDto);
+        return ResponseUtils.ok(MsgType.SIGNUP_SUCCESSFULLY);
+    }
+
+    @PostMapping("/login")
+    public ApiResponseDto<Void> login(@RequestBody LoginRequestDto requestDto, HttpServletResponse response){
+
+        usersService.login(requestDto,response);
+        return ResponseUtils.ok(MsgType.LOGIN_SUCCESSFULLY);
+    }
 }
--- server/src/main/java/team21/solsolpokect/user/service/UsersService.java
+++ server/src/main/java/team21/solsolpokect/user/service/UsersService.java
@@ -1,4 +1,54 @@
 package team21.solsolpokect.user.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import team21.solsolpokect.common.exception.CustomException;
+import team21.solsolpokect.common.exception.ErrorType;
+import team21.solsolpokect.common.jwt.JwtUtil;
+import team21.solsolpokect.user.dto.request.LoginRequestDto;
+import team21.solsolpokect.user.dto.request.SignupRequestDto;
+import team21.solsolpokect.user.entity.Users;
+import team21.solsolpokect.user.repository.UsersRepository;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
 public class UsersService {
+
+    private final UsersRepository usersRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
+
+    public void signup(SignupRequestDto requestDto) {
+
+        String encodePw = passwordEncoder.encode(requestDto.getPassword());
+
+        Optional<Users> isExist = usersRepository.findByUserIdAndDeletedAtIsNull(requestDto.getUserId());
+        if(isExist.isPresent()){
+            throw new CustomException(ErrorType.ALREADY_EXIST_USERID);
+        }
+
+        Users users = Users.of(requestDto.getUserId(), encodePw, requestDto.getRole(), requestDto.getAccount(), requestDto.getUsername());
+        usersRepository.save(users);
+    }
+
+    public void login(LoginRequestDto requestDto, HttpServletResponse response) {
+
+        String userId = requestDto.getUserId();
+        String password = requestDto.getPassword();
+
+        Users user = usersRepository.findByUserIdAndDeletedAtIsNull(userId).orElseThrow(
+                () -> new CustomException(ErrorType.NOT_MATCHING_INFO)
+        );
+
+        if (!passwordEncoder.matches(password, user.getPassword())) {
+            throw new CustomException(ErrorType.NOT_MATCHING_INFO);
+        }
+
+        String token = jwtUtil.createToken(user.getUserId());
+        response.addHeader(JwtUtil.AUTHORIZATION_HEADER, token);
+    }
 }
