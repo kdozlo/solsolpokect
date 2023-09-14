df977692e771a8353356d45bd374a31472767b07
kdozlo <eodud5730@naver.com>
2023-09-14 22:23:36 +0900

    Chore : long -> Long

--- server/src/main/java/team21/solsolpokect/mission/controller/MissionController.java
+++ server/src/main/java/team21/solsolpokect/mission/controller/MissionController.java
@@ -24,37 +24,37 @@ public class MissionController {
     private final MissionService missionService;
 
     @PostMapping("/create/{user-id}")
-    public ApiResponseDto<Void> missionCreate(@PathVariable("user-id") long userId, @RequestBody MissionCreateRequestDto missionCreateRequestDto) {
+    public ApiResponseDto<Void> missionCreate(@PathVariable("user-id") Long userId, @RequestBody MissionCreateRequestDto missionCreateRequestDto) {
         missionService.missionCreate(userId, missionCreateRequestDto);
         return ResponseUtils.ok(MsgType.MISSION_CREATE_SUCCESSFULLY);
     }
 
     @PutMapping("/allow/{mission-id}")
-    public ApiResponseDto<Void> missionAllow(@PathVariable("mission-id") long missionId, @RequestBody MissionAllowRequestDto missionAllowRequestDto) {
+    public ApiResponseDto<Void> missionAllow(@PathVariable("mission-id") Long missionId, @RequestBody MissionAllowRequestDto missionAllowRequestDto) {
         missionService.missionAllow(missionId, missionAllowRequestDto);
         return ResponseUtils.ok(missionAllowRequestDto.isAllow() ? MsgType.MISSION_ALLOW_SUCCESSFULLY : MsgType.MISSION_REJECT_SUCCESSFULLY);
     }
 
     @GetMapping("/list")
-    public ApiResponseDto<List<MissionInfosResponseDto>> missionList(@RequestParam long userId) {
+    public ApiResponseDto<List<MissionInfosResponseDto>> missionList(@RequestParam Long userId) {
 
         return ResponseUtils.ok(missionService.missionList(userId), MsgType.MISSION_LIST_SUCCESSFULLY);
     }
 
     @GetMapping("/detail/{mission-id}")
-    public ApiResponseDto<MissionInfoDetailResponseDto> missionDetail(@PathVariable("mission-id") long missionId) {
+    public ApiResponseDto<MissionInfoDetailResponseDto> missionDetail(@PathVariable("mission-id") Long missionId) {
 
         return ResponseUtils.ok(missionService.missionDetail(missionId), MsgType.MISSION_DETAIL_SUCCESSFULLY);
     }
 
     @DeleteMapping("/{mission-id}")
-    public ApiResponseDto<Void> missionDelete(@PathVariable("mission-id") long missionId) {
+    public ApiResponseDto<Void> missionDelete(@PathVariable("mission-id") Long missionId) {
         missionService.missionDelete(missionId);
         return ResponseUtils.ok(MsgType.MISSION_DELETE_SUCCESSFULLY);
     }
 
     @PutMapping("/allow-picture/{mission-id}/{user-id}")
-    public ApiResponseDto<Void> missionAllowPicture(@PathVariable("mission-id") long missionId, @PathVariable("user-id") long userId,
+    public ApiResponseDto<Void> missionAllowPicture(@PathVariable("mission-id") Long missionId, @PathVariable("user-id") long userId,
                                                     @RequestPart MultipartFile picture) throws IOException {
         System.out.println("mission id : " + missionId + "user id : " + userId);
         missionService.missionAllowPicture(missionId, userId, picture);
@@ -62,7 +62,7 @@ public class MissionController {
     }
 
     @PutMapping("/complete/{mission-id}")
-    public ApiResponseDto<Void> missionComplete(@PathVariable("mission-id") long missionId, @RequestBody MissionCompleteRequestDto missionCompleteRequestDto) {
+    public ApiResponseDto<Void> missionComplete(@PathVariable("mission-id") Long missionId, @RequestBody MissionCompleteRequestDto missionCompleteRequestDto) {
         missionService.missionComplete(missionId, missionCompleteRequestDto);
         return ResponseUtils.ok(MsgType.MISSION_COMPLETE_SUCCESSFULLY);
     }
