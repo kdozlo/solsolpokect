0df999a7ddbc5b103a5a228aa7d42152e02bdd07
Seyoung Oh <osy9536@naver.com>
2023-09-14 13:57:36 +0900

    Feat : UserInfo API 구현
    close #65

--- server/src/main/java/team21/solsolpokect/user/dto/response/UsersInfoResponseDto.java
+++ server/src/main/java/team21/solsolpokect/user/dto/response/UsersInfoResponseDto.java
@@ -1,4 +1,33 @@
 package team21.solsolpokect.user.dto.response;
 
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
 public class UsersInfoResponseDto {
+
+    private String role;
+    private String username;
+    private Long familyId;
+    private String account;
+    private int creditScore;
+
+    @Builder
+    private UsersInfoResponseDto(String role, String username, Long familyId, String account, int creditScore) {
+        this.role = role;
+        this.username = username;
+        this.familyId = familyId;
+        this.account = account;
+        this.creditScore = creditScore;
+    }
+
+    public static UsersInfoResponseDto of(String role, String username, Long familyId, String account, int creditScore) {
+        return builder()
+                .role(role)
+                .username(username)
+                .familyId(familyId)
+                .account(account)
+                .creditScore(creditScore)
+                .build();
+    }
 }
