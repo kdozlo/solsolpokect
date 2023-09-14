12cf3965809970052589e31cc98f01a00aa509ee
Seyoung Oh <osy9536@naver.com>
2023-09-14 20:03:33 +0900

    Fix : 날짜 클래스 타입 변경

--- server/src/main/java/team21/solsolpokect/common/entity/DateUtils.java
+++ server/src/main/java/team21/solsolpokect/common/entity/DateUtils.java
@@ -1,22 +1,22 @@
 package team21.solsolpokect.common.entity;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.YearMonth;
 
 public class DateUtils {
-    public static LocalDate getStartOfMonth(String year, String month) {
+    public static LocalDateTime getStartOfMonth(String year, String month) {
         int intYear = Integer.parseInt(year);
         int intMonth = Integer.parseInt(month);
 
-        YearMonth yearMonth = YearMonth.of(intYear, intMonth);
-        return yearMonth.atDay(1); // 해당 년도와 월의 1일을 반환
+        return LocalDateTime.of(intYear, intMonth, 1,0,0); // 해당 년도와 월의 1일을 반환
     }
 
-    public static LocalDate getEndOfMonth(String year, String month) {
+    public static LocalDateTime getEndOfMonth(String year, String month) {
         int intYear = Integer.parseInt(year);
         int intMonth = Integer.parseInt(month);
 
         YearMonth yearMonth = YearMonth.of(intYear, intMonth);
-        return yearMonth.atEndOfMonth(); // 해당 년도와 월의 마지막 날짜를 반환
+        return LocalDateTime.of(intYear, intMonth, yearMonth.atEndOfMonth().getDayOfMonth(),23,59); // 해당 년도와 월의 마지막 날짜를 반환
     }
 }
